c0985a3801858bd39302c1b59c48c44cda6cd1ba
Ihsan Fajri <ihsanfajri@gmail.com>
2020-12-02 16:50:50 +0700

    Menampilkan Icon Tab Bottom Navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../pages/HomeScreen'
@@ -11,16 +11,51 @@ import RegisterScreen from '../pages/RegisterScreen'
 import LoginScreen from '../pages/LoginScreen'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import iconHome from '../assets/image/home.png'
+import iconTransaksi from '../assets/image/transfer.png'
+import iconAccount from '../assets/image/user.png'
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const IconBottom = (props) => {
+  const {color, focused} = props.data
+  let colorSelected = focused ? color : '#A01F1F'
+  return(
+    <View>
+      <Image source={props.image} style={{width: 25, height: 25, tintColor: colorSelected }} />
+    </View>
+  )
+}
+
 const Dashboard = () => {
     return(
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="HistoryTicket" component={HistoryTickeetScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            tabBarLabel: () => {return null},
+            tabBarIcon: (props) => {return <IconBottom data={props} image={iconHome}/>},
+          }}
+        />
+        <Tab.Screen 
+          name="HistoryTicket" 
+          component={HistoryTickeetScreen} 
+          options={{
+            tabBarLabel: () => {return null},
+            tabBarIcon: (props) => { return <IconBottom data={props} image={iconTransaksi}/>}
+          }}
+        />
+        <Tab.Screen 
+          name="Account" 
+          component={AccountScreen}
+          options={{
+            tabBarLabel: () => {return null},
+            tabBarIcon: (props) => { return <IconBottom data={props} image={iconAccount}/>}
+          }}
+        />
       </Tab.Navigator>
 
     )
