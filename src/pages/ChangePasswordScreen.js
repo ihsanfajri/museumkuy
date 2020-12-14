78ac143a15827bbeb4ac51ffcc13e5fd8d7f9e02
Ihsan Fajri <ihsanfajri@gmail.com>
2020-12-14 08:30:01 +0700

    memperbaiki tampilan Ubah password dan Profile

--- src/pages/ChangePasswordScreen.js
+++ src/pages/ChangePasswordScreen.js
@@ -1,9 +1,8 @@
-import React,{useState} from 'react'
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Modal, TouchableHighlight,ScrollView } from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native'
+
 import logoChangePassword from '../assets/image/changepassword.png'
 import * as Animatable from 'react-native-animatable'
-
-import ChangePassword from '../components/ChangePassword'
 
 
 const ChangePasswordScreen = ({navigation}) => {
@@ -11,7 +10,28 @@ const ChangePasswordScreen = ({navigation}) => {
     return (
         <ScrollView style={styles.containerScroll}> 
            <View style={styles.containerContent}>
-               <ChangePassword  />
+                <View style={styles.containerTitle}>
+                    <Text style={styles.Title}>Ubah Password</Text>
+                </View>
+                <View style={styles.containerForm}>
+                    <View style={styles.containerInputText}>
+                            <Text style={styles.fontInputText}>Masukkan Password Lama</Text>
+                            <TextInput style={styles.inputText}/>
+                    </View>
+                    <View style={styles.containerInputText}>
+                            <Text style={styles.fontInputText}>Masukkan Password Baru</Text>
+                            <TextInput style={styles.inputText} />
+                    </View>
+                    <View style={styles.containerInputText}>
+                            <Text style={styles.fontInputText}>Konfirmasi Password Baru</Text>
+                            <TextInput style={styles.inputText} />
+                    </View>
+                </View>
+                <View style={styles.containerBtn}>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.fontBtn}>Ubah Password</Text>
+                    </TouchableOpacity>
+                </View>
            </View>
         </ScrollView>
     )
@@ -26,6 +46,67 @@ const styles = StyleSheet.create({
         width: '95%',
         height: '100%',
         // borderWidth: 1,
+    },
+
+    containerTitle:{
+        // borderWidth: 1,
+        marginHorizontal: 10,
+        // marginVertical: 10,
+        marginBottom: 40,
+        marginTop: 50,
+    },
+
+    Title:{
+        fontSize: 20,
+        marginLeft: 10,
+        fontWeight: "bold",
+    },
+
+    containerForm:{
+        marginHorizontal: 10,
+        marginVertical: 10,
+        // borderWidth: 1,
+    },
+
+    containerInputText:{
+        marginHorizontal: 10,
+        marginVertical: 10,
+        // borderWidth: 1,
+    },
+
+    inputText:{
+        borderBottomWidth: 1,
+        // borderWidth:1,
+        marginTop: 10,
+        height: 40,
+    },
+
+    fontInputText:{
+        color: '#9D9D9D',
+        fontWeight: "bold",
+    },
+
+    containerBtn:{
+        marginHorizontal: 30,
+        marginVertical: 20,
+        height: 45,
+        // borderWidth: 1,
+    },
+
+    btn:{
+        // borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#A01F1F',
+        borderRadius: 5,
+    },
+
+    fontBtn:{
+        fontWeight: "bold",
+        color: 'white',
+        fontSize: 14,
     }
     
     
--- src/pages/ProfileUpdateScreen.js
+++ src/pages/ProfileUpdateScreen.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import UpdateProfile from '../components/UpdateProfile'
+import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native'
+
+import FotoProfil from '../assets/image/mariavania.jpeg'
+import Camera from '../assets/logo/camera.png'
 
 const ProfileUpdateScreen = () => {
     return (
        
        <ScrollView style={styles.container}>
-           <View style={styles.containerContent}>
-            <   UpdateProfile />
-           </View>
+           <View style={styles.containerFoto}>
+                <Image source={FotoProfil} style={styles.foto} />
+            </View>
+            <View style={styles.containerForm}>
+                <View style={styles.containerBingkai}>
+                    <View style={styles.containerInputText}>
+                        <Text style={styles.fontTextInput}>Nama Lengkap</Text>
+                        <TextInput style={styles.InputText}/>
+                    </View>
+                    <View style={styles.containerInputText}>
+                        <Text style={styles.fontTextInput}>Email</Text>
+                        <TextInput style={styles.InputText}/>
+                    </View>
+                    <View style={styles.containerInputText}>
+                        <Text style={styles.fontTextInput}>No.Telp</Text>
+                        <TextInput style={styles.InputText} />
+                    </View>
+                    <View style={styles.containerBtn}>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={styles.fontbtn}>Ubah Profile</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.containerbtnFoto}>
+                <TouchableOpacity style={styles.btnFotoCam}>
+                    <Image source={Camera} style={styles.fotoCam} />
+                </TouchableOpacity>
+            </View>
        </ScrollView>
     )
 }
@@ -18,11 +46,132 @@ export default ProfileUpdateScreen
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        // backgroundColor: 'green',
+    },
+    containerFoto:{
+        flex: 0.7,
+        height: 225,
     },
 
-    containerContent:{
+    foto:{
+        resizeMode: 'stretch',
+        height: '100%',
+        width: '100%',
+    },
+
+    containerbtnFoto: {
         flex: 1,
-        // backgroundColor: 'blue',
+        position: 'absolute',
+        // backgroundColor: '#C4C4C4',
+        // borderWidth: 1,
+        width:'100%',
+        height: '43%',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+    },
+
+    btnFotoCam:{
+        width: 70,
+        height: 70,
+        backgroundColor:'white',
+        borderRadius: 70/2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // marginBottom: 20,
+        marginRight: 20,
+        // marginBottom: 120,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+
+    fotoCam:{
+        width: '70%',
+        height: '60%'
+    },
+
+    containerForm:{
+        flex: 1,
+        backgroundColor: 'white',
+        // height: '100%', 
+        width: '100%'
+
+    },
+
+    containerBingkai:{
+        // borderWidth: 1,
+        backgroundColor: 'white',
+        width: '97%',
+        height: 375,
+        alignSelf: 'center',
+        // marginVertical: 10,
+        marginTop: 10, 
+        marginBottom: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 8,
+        },
+        shadowOpacity: 0.46,
+        shadowRadius: 11.14,
+
+        elevation: 17,
+    },
+
+    containerInputText:{
+        marginTop: 10,
+        marginHorizontal: 30,
+    },
+
+    fontTextInput:{
+        color: '#9D9D9D',
+        fontWeight: "bold",
+    },
+
+    InputText:{
+        borderBottomWidth: 1,
+    },
+
+    containerBtn:{
+        marginVertical: 30,
+        // borderWidth: 1,
+        width: '75%',
+        height: 45,
+        alignSelf: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+
+    btn:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        // marginBottom: 10,
+        // marginLeft: 10,
+        // marginRight: 10,
+        // marginTop: 10,
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#A01F1F',
+        borderRadius: 5,
+        marginTop: 10, 
+        
+    },
+
+    fontbtn:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 })
